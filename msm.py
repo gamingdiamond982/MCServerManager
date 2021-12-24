63018100423c5c075fa96d76e319381f1b8ff7e3
Ash Johnston <oj.nologic@gmail.com>
2021-12-24 14:29:05 +0000

    some argparse stuff`

--- msm.py
+++ msm.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
 import argparse
 import re
+from enum import Enum
+
+
+
+class ServerType(Enum):
+    VANILLA = 0
+    BUKKIT  = 1
+    SPIGOT  = 2
+    PAPER   = 3
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def from_string(cls, s):
+        try:
+            return cls[s.upper()]
+        except KeyError:
+            raise ValueError()
+
 
 def create_server(args):
     print(args.__dict__)
+    
     raise NotImplementedError()
 
 def list_servers(args):
@@ -26,23 +47,25 @@ def restart_server(args):
     stop_server(args)
     start_server(args)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A command line utility that manages your minecraft server for you')
+    parser.set_defaults(func=lambda args: None)
     subparsers = parser.add_subparsers(title='Commands', metavar='<command>')
     
     server_info_parser = argparse.ArgumentParser(add_help=False)
     server_info_parser.add_argument('name', help='The name of the server')
 
     # add server parser stuff
-    add_server_parser = subparsers.add_parser('create-server', aliases=['cs'], parents=[server_info_parser], help='Creates a server')
+    add_server_parser = subparsers.add_parser('create', parents=[server_info_parser], help='Creates a server')
     add_server_parser.set_defaults(func=create_server)
     add_server_parser.add_argument('-j', '--jar', help='The server jar to use when building the server')
-    add_server_parser.add_argument('-t', '--server-type', choices=['vanilla', 'bukkit', 'spigot', 'paper'], default='paper', help='The type of server to use when building a jar, defaults to paper')
+    add_server_parser.add_argument('-t', '--server-type', choices=list(ServerType), type=ServerType.from_string, default=ServerType.PAPER, help='The type of server to use when building a jar, defaults to paper')
     add_server_parser.add_argument('-v', '--mc-version', default='latest', help='The version of minecraft to use for the server')
     add_server_parser.add_argument('-b', '--build-number', default='latest', help='specify the build number if a particular build is wanted, defaults to latest')
     
     # list servers parser stuff
-    subparsers.add_parser('list-servers', aliases=['ls'], help='Lists the existing minecraft servers').set_defaults(func=list_servers)
+    subparsers.add_parser('list', help='Lists the existing minecraft servers').set_defaults(func=list_servers)
     
     # remove server parser stuff
     subparsers.add_parser('remove', parents=[server_info_parser], help='Removes a server').set_defaults(func=remove_server)   
